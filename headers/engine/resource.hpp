11c813a206537f632d9186a9598fa36a03bad940
Selandron <etienne.andrieu@insa-cvl.fr>
2018-06-09 22:40:15 +0200

    Add Resource Abstract class

---
+++ headers/engine/resource.hpp
@@ -0,0 +1,55 @@
+#ifndef RESOURCE_HPP
+#define RESOURCE_HPP
+
+/*
+    Virtual class representing a Resource. Can be any type of resource listed in RESOURCE_TYPE,
+    but every type need to be implemented in a child class, and 
+
+    Author : Etienne Andrieu
+*/
+
+#include <string>
+
+namespace tr
+{
+
+typedef enum
+{
+    RESOURCE_NULL = 0,
+    RESOURCE_GRAPHIC = 1,
+    RESOURCE_MOVIE = 2,
+    RESOURCE_AUDIO = 3,
+    RESOURCE_TEXT = 4,
+    RESOURCE_FONT = 5,
+}RESOURCE_TYPE; //Define every type of resource possible
+
+class Resource
+{
+    public:
+        Resource() : m_resourceID("null"), m_filename("null"), m_type(RESOURCE_NULL), m_loaded(false) {}        //Constructor
+        virtual ~Resource() {}                                                                                  //Destructor
+        virtual void Load() = 0;                                                                                //Loading function (need to be implemented)
+        virtual void Unload() = 0;                                                                              //Unload function (need to be implemented)
+
+        //Accessors and mutators (careful, only changing ID will no unload resource)
+        void SetResourceID(std::string ID) { m_resourceID = ID; }
+        std::string GetResourceID() const { return m_resourceID; }
+
+        void SetFilename(std::string filename) { m_loaded = false; m_filename = filename; }
+        std::string GetFilename() const { return m_filename; }
+
+        void SetResourceType(RESOURCE_TYPE type) { m_loaded = false; m_type = type; }
+        RESOURCE_TYPE GetResourceType() const { return m_type; }
+
+        bool IsLoaded() const { return m_loaded; }
+
+    protected:
+        std::string m_resourceID;
+        std::string m_filename;
+        RESOURCE_TYPE m_type;
+        bool m_loaded;
+    private:
+};
+
+}
+#endif
